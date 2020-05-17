66c1ff4e435ee00410270a8a6982007a0aa0cabe
Korran <korranhorn009@gmail.com>
2020-05-17 10:12:21 +0300

    Add Creative ViewPager

--- app/src/main/java/ru/mertsalovda/travelapp/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/mertsalovda/travelapp/ui/search/SearchFragment.kt
@@ -1,31 +1,37 @@
 package ru.mertsalovda.travelapp.ui.search
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fr_search.*
 import ru.mertsalovda.travelapp.R
 
 class SearchFragment : Fragment() {
 
     private lateinit var searchViewModel: SearchViewModel
-
+    private lateinit var adapter: CityViewPagerAdapter
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         searchViewModel =
-                ViewModelProviders.of(this).get(SearchViewModel::class.java)
-        val root = inflater.inflate(R.layout.fr_search, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_search)
-        searchViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-        })
-        return root
+            ViewModelProviders.of(this).get(SearchViewModel::class.java)
+        return inflater.inflate(R.layout.fr_search, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        adapter = CityViewPagerAdapter(context)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewPager.setCreativeViewPagerAdapter(adapter)
+        tvSize.text = "/" + adapter.getCount()
     }
 }
